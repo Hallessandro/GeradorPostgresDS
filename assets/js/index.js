5369e540bae4d8d745e49d37af4bf81fae4438c5
Hallessandro D´villa <hallessandro.silva@academico.ifrn.edu.br>
2018-07-24 08:07:05 -0300

    Versão 1.0 - Finalizada

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,7 @@
 var fs = require('fs');
+var path = require("path");
+var app = require('electron').remote; 
+var dialog = app.dialog;
 
 const $ = document.querySelector.bind(document);
 const CAMINHO_JBOSS_5 = "";
@@ -35,7 +38,7 @@ document.querySelector("#form-dados").addEventListener("submit", function(event)
         let resultado = montarArquivo(bancoAdm.value, usrAdm.value, senhaAdm.value, bancoSigaa.value, usrSigaa.value, senhaSigaa.value, 
                 bancoComum.value, usrComum.value, senhaComum.value, serverName.toLowerCase(), porta.value, cliente.value.toLowerCase());
         console.log(resultado);
-        gerarArquivo(resultado, pastaJboss.files[0].path);
+        gerarArquivo(resultado);
     }
 });
 
@@ -59,17 +62,24 @@ function selecionarVersaoJboss(){
     }
 }
 
-function gerarArquivo(conteudo, caminho){
-    fs.writeFile(caminho, conteudo, (err) => {
-        if (err) {            
-            res = '<div class="alert alert-danger" role="alert"> Deu ruim: ' + err +'</div>';
-            document.getElementById("erros").innerHTML = res;
-        }else {            
-            res = '<div class="alert alert-success" role="alert">Deu bom demais: Arquivo gerado com sucesso!</div>';
-            document.getElementById("erros").innerHTML = "Deu bom:";
+function gerarArquivo(conteudo){
+    filename = 'teste.xml';
+    // You can obviously give a direct path without use the dialog (C:/Program Files/path/myfileexample.txt)
+    dialog.showSaveDialog((fileName) => {
+        if (fileName === undefined){
+            console.log("You didn't save the file");
+            return;
         }
-        
-    });
+
+        // fileName is a string that contains the path and filename created in the save file dialog.  
+        fs.writeFile(fileName, conteudo, (err) => {
+            if(err){
+                alert("Deu ruim: "+ err.message)
+            }
+                        
+            alert("O arquivo foi salvo com sucesso!");
+        });
+    }); 
 }
 
 function populaCampos(){
@@ -102,12 +112,15 @@ function populaCampos(){
         }
     }else if(servidor.value === "bdrestauracao"){
         porta.value = "5432";
-        bancoAdm.value = ""
-        bancoComum.value = "";
-        bancoSigaa.value = "";
+        bancoAdm.value = cliente.value.toLowerCase() + "_administrativo_2018";
+        bancoComum.value = cliente.value.toLowerCase() + "_sistemas_comum_2018";
+        bancoSigaa.value = cliente.value.toLowerCase() + "_sigaa_2018";
         serverName.value = "bdrestauracao";
     }else {
         porta.value = "";
+        bancoAdm.value = ""
+        bancoComum.value = "";
+        bancoSigaa.value = "";
     }
 
     if(servidor.value === "preprod" || servidor.value === "bdrestauracao"){
@@ -128,13 +141,9 @@ function populaCampos(){
 }
 
 function validaCampos(){
-    let versao = $("#versaoJboss");    
     let cliente = $("#cliente");
     let servidor = $("#servidor");    
     let erros = [];
-    if(!versao.value || versao.value === "0"){
-        erros.push("Jboss: Campo obrigatório não informado!");
-    }
     if(!cliente.value){
         erros.push("Cliente: Campo obrigatório não informado")
     }
